a1a629c03e6f6b8bf12efbe8b8619086bb49111d
Soshin Aleksandr <Tappok939393@gmail.com>
2024-11-02 10:03:06 +0300

    Возврат анимации, добавление Json файла со схемой таблицы БД.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,15 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments["room.schemaLocation"] = "$projectDir/schemas"
+            }
+        }
     }
+
+
 
     buildTypes {
         release {
@@ -52,30 +60,30 @@ dependencies {
     val dagger_version = "2.52"
 
     //Dagger 2
-    implementation ("com.google.dagger:dagger:$dagger_version")
-    annotationProcessor ("com.google.dagger:dagger-compiler:$dagger_version")
-    kapt ("com.google.dagger:dagger-compiler:$dagger_version")
-    implementation ("javax.inject:javax.inject:1")
+    implementation("com.google.dagger:dagger:$dagger_version")
+    annotationProcessor("com.google.dagger:dagger-compiler:$dagger_version")
+    kapt("com.google.dagger:dagger-compiler:$dagger_version")
+    implementation("javax.inject:javax.inject:1")
 
     //Room database
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
 
     //Navigation
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.6.0")
-    implementation ("com.google.android.material:material:1.9.0")
-    implementation ("androidx.compose.runtime:runtime:1.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.compose.runtime:runtime:1.5.1")
 
     //RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.0")
 
     //Push уведомления
-    implementation ("androidx.core:core-ktx:1.10.1")
-    implementation ("androidx.core:core:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core:1.9.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
